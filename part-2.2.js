fa51e0e7782bebb49737e21b34bf3ba0ffb4d50e
wira sanjaya <sanjayawira02@gmail.com>
2023-07-28 08:58:57 +0700

    part awal

---
+++ part-2.2.js
@@ -0,0 +1,13 @@
+// cara masukan baju ke mesin cuci
+// misal mesin cuci hanya muat 3 baju
+// lalu kita butuh mencuci 4 baju
+
+// langkah awal 
+// kita masukkan 3 baju sekaligus ke mesin cuic tersebut 
+// kalau kita memasukkan 4 baju sekaligus maka akan Ada 
+// peringatan dari mesin cuci  tersebut
+
+// lalu kalau sudah memasukkan 3 baju atau krang dari Itu 
+// baju kotor dikurangi baju yang masuk ke mesin cuci
+
+// lalu lakukan hal tersebut sampai baju kotor habis
